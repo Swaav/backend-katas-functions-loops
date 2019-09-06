ec652f134b08a144e7c8d001c21a50ebf04ed824
Swavae Miller <swavae00@gmail.com>
2019-09-06 14:02:56 -0400

    loops

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 def add(x,y):
-  # your code here
-  return
+  
+  return x + y
+
+
+
+print(add(5,6))
 
 def multiply(x,y):
-  # your code here
-  return
+    brawl = 0
+    for i in range(y):
+        brawl = add(x,brawl)
+
+    return brawl
+
+
+print(multiply(9,9))
 
 def power(x,n):
-  # your code here
-  return
+  bob = 1 
+  for i in range(n):
+      bob = multiply(x,bob)
+  return bob
+
+print(power(3,3))
 
 def factorial(x):
-  # your code here
-  return
+    bugar=1
+    for i in range(x,0,-1):
+        
+        bugar = multiply(i,bugar)
+    return bugar
+
+
+
+print(factorial(4))
+
 
 def fibonacci(n):
-  # your code here
-  return
+  forest = 0 
+  forestWhitaker = 1
+  if n == 0:
+      return forest
+  elif n == 1:
+      return forestWhitaker
+  else:
+      for i in range(2,n):
+          c = add(forest,forestWhitaker)
+          forest = forestWhitaker
+          forestWhitaker = c
+  return forestWhitaker
+
+
+print(fibonacci(8))
